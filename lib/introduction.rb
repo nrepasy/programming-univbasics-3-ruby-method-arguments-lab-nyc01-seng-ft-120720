2fd0acbbecc1bd8a91c21b33685ed790db7a3c1c
nick repasy <nrepasy@gmail.com>
2020-11-23 20:36:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,8 +5,10 @@ end
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
   
-  
-
+language = "Ruby"  
+  def introduction_with_language_optional( name)
+    put "Hi, my name is #{name} and I am learning to program in #{language}."
+  end
 
 
 
